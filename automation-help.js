f52219f706b60b60be9e6c0c34f9876a4f50e80e
tshhhsilence <gsailent29@gmail.com>
2025-08-19 11:58:35 +0300

    Create automation-help.js

---
+++ automation-help.js
@@ -0,0 +1,22 @@
+function waitForZoneJs() {
+  return new Promise((resolve) => {
+    const checkZone = () => {
+      if (typeof Zone !== 'undefined' && Zone.current) {
+        resolve();
+      } else {
+            console.log('Zone.js !!!', window.Zone);
+        // Проверяем каждые 10мс
+        setTimeout(checkZone, 10);
+      }
+    };
+    checkZone();
+  });
+}
+
+// Использование
+waitForZoneJs().then(() => {
+  console.log('Zone.js инициализирован');
+      document.addEventListener('click', (event) => {
+        event.stopImmediatePropagation()
+    });
+});
